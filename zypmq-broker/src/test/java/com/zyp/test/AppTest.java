4c6d82192733743ef5f368e6b79fd3e874877029
zhangyipeng <67528767+ZypIsGod@users.noreply.github.com>
2026-03-28 18:56:19 +0800

    第一次提交

---
+++ zypmq-broker/src/test/java/com/zyp/test/AppTest.java
@@ -0,0 +1,12 @@
+package com.zyp.test;
+
+/**
+ * @Date:2026/3/28
+ * @Author：zyp
+ * @Description:
+ */
+public class AppTest {
+    public static void main(String[] args) {
+            System.out.println("hello world");
+    }
+}
