d21d2048b57c7b794441094460b98bbfc0c35cd8
zhangyipeng <67528767+ZypIsGod@users.noreply.github.com>
2026-04-03 08:44:03 +0800

    提交

---
+++ zypmq-broker/src/main/java/com/zyp/mq/broker/config/GlobalProperties.java
@@ -0,0 +1,19 @@
+package com.zyp.mq.broker.config;
+
+/**
+ * @Date:2026/4/3
+ * @Author：zyp
+ * @Description:
+ */
+public class GlobalProperties {
+
+    private String zypMqHome;
+
+    public String getZypMqHome() {
+        return zypMqHome;
+    }
+
+    public void setZypMqHome(String zypMqHome) {
+        this.zypMqHome = zypMqHome;
+    }
+}
